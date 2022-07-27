4d53bcece463a8440a6b35e5f26b93efa8edadf3
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-07-27 12:59:32 +0530

    Time: 13 ms (64.44%), Space: 55.5 MB (25.37%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,31 +1,45 @@
-
 class Solution {
     public int[][] merge(int[][] intervals) {
         
-        ArrayList<int[]> list=new ArrayList<>();
-    
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        
-        
-        int start=intervals[0][0];
-        int end=intervals[0][1];
-        
-        
-        for(int[] i:intervals){
-            
-            if(i[0]<=end){
-                end=Math.max(end,i[1]);
+ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();             
+Arrays.sort(intervals,(a,b) -> Integer.compare(a[0],b[0]));
+		
+        int j=0;
+        int k=0;
+        for(int i=0;i<intervals.length;i++){
+            if(i==0&&j==0){
+                list.add(new ArrayList<Integer>());
+                list.get(k).add(intervals[i][0]);
+                list.get(k).add(intervals[i][1]);
+                continue;
+            }
+            if(list.get(k).get(1)>=intervals[i][0]){
+                if(list.get(k).get(0)>=intervals[i][0]){
+                    list.get(k).set(0,intervals[i][0]);
+                    
+                }
+                if(list.get(k).get(1)>intervals[i][1])
+                    continue;
+                list.get(k).set(1,intervals[i][1]);
+                
+                
             }
             else{
-                list.add(new int[]{start,end});
-                start=i[0];
-                end=i[1];
+                k++;
+                list.add(new ArrayList<Integer>());
+                list.get(k).add(intervals[i][0]);
+                list.get(k).add(intervals[i][1]);
+                
             }
             
+            
         }
-        
-        list.add(new int[]{start,end});
-        return list.toArray(new int[0][]);
+        int[][]arr=new int[list.size()][2];
+        for(int i=0;i<list.size();i++){
+            arr[i][0]=list.get(i).get(0);
+            arr[i][1]=list.get(i).get(1);
+        }
+            return arr;
         
     }
 }
