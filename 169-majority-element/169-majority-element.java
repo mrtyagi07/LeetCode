77be741ca023e280ec9a1234973fa6670dd09690
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-07-31 18:57:03 +0530

    Time: 1 ms (100.00%), Space: 46.1 MB (91.63%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -17,19 +17,40 @@
 //     }
 // }
 
-public class Solution {
-    public int majorityElement(int[] num) {
+// public class Solution {
+//     public int majorityElement(int[] num) {
 
-        int major=num[0], count = 1;
-        for(int i=1; i<num.length;i++){
-            if(count==0){
-                count++;
-                major=num[i];
-            }else if(major==num[i]){
-                count++;
-            }else count--;
+//         int major=num[0], count = 1;
+//         for(int i=1; i<num.length;i++){
+//             if(count==0){
+//                 count++;
+//                 major=num[i];
+//             }else if(major==num[i]){
+//                 count++;
+//             }else count--;
             
+//         }
+//         return major;
+//     }
+// }
+
+class Solution {
+    public int majorityElement(int[] nums) {
+     int number=nums[0];
+	int count =1;
+
+	for(int i=1;i<nums.length;i++){
+		if(nums[i]==number){
+			count++;
+		}
+		else if(nums[i]!=number && count>0){
+			count--;
+		}else if(count<=0){
+			number= nums[i];
+			count=1;
+		}
+	}
+
+	return number;
         }
-        return major;
     }
-}
