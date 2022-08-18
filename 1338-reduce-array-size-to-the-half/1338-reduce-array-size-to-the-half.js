8de156a9338e87c328bf3da5f4b72c293402cd0b
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-08-18 16:13:01 +0530

    Time: 185 ms (71.70%), Space: 58.6 MB (68.87%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.js
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.js
@@ -1,33 +1,13 @@
 var minSetSize = function(arr) {
-    let res = [];
-    for (let n of arr) {
-        if (!res[n]) {
-            res[n] = 1;
-        } else {
-            res[n] = res[n] + 1;
-        }
-    }
-    res.sort((a, b) => b - a);
-    let count =  arr.length;
-    const half = parseInt(arr.length / 2);
-    
-    let i = 0;
-    for (; i < res.length; i++) {
-        count -= res[i];
-        if ( count <= half) break;
-    }
-    
-    return i + 1;
-};
-// var minSetSize = function (arr) {
-//   const numFreq = {};
-//   let count = 0;
-//   for (let i = 0; i < arr.length; i++) numFreq[arr[i]] ? numFreq[arr[i]]++ : (numFreq[arr[i]] = 1);
-
-//   const sortedIntegerFeq = Object.values(numFreq).sort((a, b) => b - a);
-
-//   for (let i = 0; i < sortedIntegerFeq.length; i++) {
-//     count += sortedIntegerFeq[i];
-//     if (count >= arr.length / 2) return i + 1;
-//   }
-// };
+  let half = arr.length / 2
+  if (half === 1) return 1
+  const counts = {}
+  for (let n of arr) {
+    if (counts[n] === undefined) counts[n] = 1
+    else if (++counts[n] >= half) return 1
+  }
+  const aggr = Object.values(counts).sort((a, b) => a - b)
+  let i = aggr.length;
+  while (half > 0) half -= aggr[--i]
+  return aggr.length - i
+}
