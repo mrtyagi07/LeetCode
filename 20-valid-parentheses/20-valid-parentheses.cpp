02f226e17e611358fb8253b56117eac67ef22a43
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-08-18 16:27:05 +0530

    Time: 8 ms (5.31%), Space: 6.4 MB (10.18%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,24 +1,15 @@
-const isValid = (s) => {
-    const map = {
-        '(':')',
-        '{':'}',
-        '[':']',        
-    }
-    let stack = []
-    for (let c of s) {
-        if (c in map){ 
-            stack.push(c)
-        } else if (map[stack.pop()] !== c) {
-            return false
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto i:s) 
+        {
+            if(i=='(' or i=='{' or i=='[') st.push(i);
+            else {
+              if(st.empty() or (st.top()=='(' and i!=')') or (st.top()=='{' and i!='}') or (st.top()=='[' and i!=']')) return false;
+                st.pop();
+            }
         }
+        return st.empty(); 
     }
-    return !stack.length
 };
-
-
-
-
-// function isValid(s, r = /\(\)|\[\]|\{\}/g) {
-//     while (s.match(r)) s = s.replace(r, '')
-//     return !s
-// }
