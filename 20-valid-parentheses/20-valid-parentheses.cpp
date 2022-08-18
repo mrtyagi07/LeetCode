2c1fa29c7e6d7a5b87b5163090a11b5d8c6207a6
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-08-18 16:24:18 +0530

    Time: 127 ms (14.70%), Space: 42.5 MB (56.38%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,26 +1,24 @@
-class Solution {
-public:
-    bool isValid(string s) {
-        stack<char> temp;
-        
-        for(int i=0; i<s.size(); i++){
-            if(temp.empty()){  
-                temp.push(s[i]);     //pushing first element into stack
-            }
-            
-            else if((temp.top() == '(' && s[i]==')') ||
-                    (temp.top() == '[' && s[i]==']') ||
-                    (temp.top() == '{' && s[i]=='}')){  
-					// comparing the next element with the top element in stack
-                temp.pop();  //if it is a pair then remove the element from stack
-            }
-            else
-                temp.push(s[i]);  
-        }
-        
-        if(temp.empty()){   // if all the pairs have been formed then return true
-            return true;
-        }
-        return false;
+const isValid = (s) => {
+    const map = {
+        '(':')',
+        '{':'}',
+        '[':']',        
     }
+    let stack = []
+    for (let c of s) {
+        if (c in map){ 
+            stack.push(c)
+        } else if (map[stack.pop()] !== c) {
+            return false
+        }
+    }
+    return !stack.length
 };
+
+
+
+
+// function isValid(s, r = /\(\)|\[\]|\{\}/g) {
+//     while (s.match(r)) s = s.replace(r, '')
+//     return !s
+// }
