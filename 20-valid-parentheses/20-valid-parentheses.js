e493bb2bd801782b35b1d91f5a164c6215347864
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-08-18 16:22:45 +0530

    Time: 127 ms (14.70%), Space: 42.5 MB (56.38%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -1,4 +1,24 @@
-function isValid(s, r = /\(\)|\[\]|\{\}/g) {
-    while (s.match(r)) s = s.replace(r, '')
-    return !s
-}
+const isValid = (s) => {
+    const map = {
+        '(':')',
+        '{':'}',
+        '[':']',        
+    }
+    let stack = []
+    for (let c of s) {
+        if (c in map){ 
+            stack.push(c)
+        } else if (map[stack.pop()] !== c) {
+            return false
+        }
+    }
+    return !stack.length
+};
+
+
+
+
+// function isValid(s, r = /\(\)|\[\]|\{\}/g) {
+//     while (s.match(r)) s = s.replace(r, '')
+//     return !s
+// }
