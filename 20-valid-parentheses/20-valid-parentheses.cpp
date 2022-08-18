6d82c07b87f4ffb4f324a08f1c57a8c1e321788c
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-08-18 16:21:53 +0530

    Time: 4 ms (32.63%), Space: 6.4 MB (51.24%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='[') st.push(s[i]);
-			else{
-                if(!st.empty()){
-                    char c=st.top();
-                    if( (s[i]==')' && c=='(')||(s[i]=='}' && c=='{')||(s[i]==']' && c=='[')){
-                        st.pop();
-                    }
-                    else return false;
-                }
-                else return false;
+        stack<char> temp;
+        
+        for(int i=0; i<s.size(); i++){
+            if(temp.empty()){  
+                temp.push(s[i]);     //pushing first element into stack
             }
+            
+            else if((temp.top() == '(' && s[i]==')') ||
+                    (temp.top() == '[' && s[i]==']') ||
+                    (temp.top() == '{' && s[i]=='}')){  
+					// comparing the next element with the top element in stack
+                temp.pop();  //if it is a pair then remove the element from stack
+            }
+            else
+                temp.push(s[i]);  
         }
-        if(st.empty()) return true;
-        else return false;
+        
+        if(temp.empty()){   // if all the pairs have been formed then return true
+            return true;
+        }
+        return false;
     }
 };
