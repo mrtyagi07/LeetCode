8f6c4bd0beab8138ca7d76ff799e619893667b55
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-08-19 17:19:28 +0530

    Time: 2 ms (98.86%), Space: 66.7 MB (82.27%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.java
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.java
@@ -1,44 +1,40 @@
 class Solution {
     public boolean isPossible(int[] nums) {
-        HashMap<Integer,Integer> avaibilityMap = new HashMap<>();
-        
-        HashMap<Integer,Integer> wantMap = new HashMap<>();
-        
-        for(int i : nums){
-            avaibilityMap.put(i, avaibilityMap.getOrDefault(i,0)+1);
-        }
-        
-        for(int i=0;i<nums.length;i++){
-         
-            if(avaibilityMap.get(nums[i])<=0){
-                continue;
-            }
-            
-            else if(wantMap.getOrDefault(nums[i],0)>0){
-         avaibilityMap.put(nums[i], avaibilityMap.getOrDefault(nums[i],0)-1);
-                
-                wantMap.put(nums[i], wantMap.getOrDefault(nums[i],0)-1);
-                
-                wantMap.put(nums[i]+1, wantMap.getOrDefault(nums[i]+1,0)+1);
-            }
-            
-            else if(
-                avaibilityMap.getOrDefault(nums[i],0)>0 &&                                   avaibilityMap.getOrDefault(nums[i]+1,0)>0 &&                                 avaibilityMap.getOrDefault(nums[i]+2,0)>0){
-                
-                avaibilityMap.put(nums[i],                avaibilityMap.getOrDefault(nums[i],0)-1);
-                avaibilityMap.put(nums[i]+1, avaibilityMap.getOrDefault(nums[i]+1,0)-1);
-                avaibilityMap.put(nums[i]+2, avaibilityMap.getOrDefault(nums[i]+2,0)-1);
-                
-              
-                wantMap.put(nums[i]+3, wantMap.getOrDefault(nums[i]+3,0)+1);
-            }
-            
-           
-            else{
-                return false;
+        int c1 = 1, c2 = 0, c3 = 0; // curr val, len=1, len=2, len=3+
+        int p1 = 0, p2 = 0, p3 = 0; // prev val, len=1, len=2, len=3+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i-1]) {
+                // curr remains the same
+                if (p1 > 0) {
+                    p1--; c2++;
+                } else if (p2 > 0) {
+                    p2--; c3++;
+                } else if (p3 > 0) {
+                    p3--; c3++;
+                } else {
+                    c1++;
+                }
+            } else if (nums[i] == nums[i-1] + 1) {
+                // curr + 1, old curr becomes prev
+                if (p1 > 0 || p2 > 0) return false;
+                p1 = c1; p2 = c2; p3 = c3;
+                c1 = 0; c2 = 0; c3 = 0;
+                if (p1 > 0) {
+                    p1--; c2++;
+                } else if (p2 > 0) {
+                    p2--; c3++;
+                } else if (p3 > 0) {
+                    p3--; c3++;
+                } else {
+                    c1++;
+                }
+            } else {
+                // curr jumps, old curr and old prev should be deprecated
+                if (c1 > 0 || c2 > 0) return false;
+                p1 = p2 = p3 = c1 = c2 = c3 = 0;
+                c1 = 1;
             }
         }
-        
-        return true;
+        return c1 == 0 && c2 == 0;
     }
 }
