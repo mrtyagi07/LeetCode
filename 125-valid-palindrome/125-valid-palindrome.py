64bcf08611a89d73f705ef5cc9c514992b46b376
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-08-22 21:03:09 +0530

    Time: 124 ms (9.94%), Space: 14.5 MB (57.17%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,14 +1,22 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        rev=""
-        reversed_string=""
-        
-        sliced_string="".join(filter(str.isalnum, s)).lower()
-
-        reversed_string=sliced_string[::-1]
-
-        if reversed_string==sliced_string:
+class Solution(object):
+    def isPalindrome(self, s):
+        i = 0
+        j = -1
+        length = len(s)
+        if length == 1:
             return True
-        else:
-            return False
         
+        while i != length+j:
+            if i > length - 1:
+                return True
+            if not s[i].isalnum():
+                i += 1
+            elif not s[j].isalnum():
+                j -= 1
+            else:
+                if s[i].lower() != s[j].lower():
+                    return False
+                i += 1
+                j -= 1
+        
+        return True
