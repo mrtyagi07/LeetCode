7da25cf7229e87e9f6e2549279455edadefd5962
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-08-22 16:04:38 +0530

    Time: 2 ms (65.34%), Space: 41.1 MB (73.02%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,9 +1,18 @@
-class Solution {
-    public boolean isPowerOfFour(int n) {
-        double pow = Math.log(n)/Math.log(4);
-        return pow==(int)pow;
-    }
+class Solution{
+public boolean isPowerOfFour(int n) {
+int fac =0b01010101010101010101010101010101;
+return n>0 && (n&(n-1))==0 && (fac&n)>0;
+ }
 }
+
+
+// class Solution {
+//     public boolean isPowerOfFour(int n) {
+//         double pow = Math.log(n)/Math.log(4);
+//         return pow==(int)pow;
+//     }
+// }
+
 
 // class Solution {
 //     public boolean isPowerOfFour(int n) {
