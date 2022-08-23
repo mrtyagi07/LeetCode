247f727130d8bb7ee78f7f793e1c0cd9cf633e1e
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-08-23 20:59:45 +0530

    Time: 270 ms (85.01%), Space: 110.2 MB (99.30%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
-bool isPalindrome(ListNode* head) 
-{
-    ListNode *node=head,*pt1,*pt2,*pt3;
-    if(!node) return true;
-    int num=0,iter;
-    while(node) {num++;node=node->next;}
-    if(num<2) return true;
-    pt1=head;
-    pt2=pt1->next;
-    pt1->next=NULL;
-    iter=num%2? (num-1)/2 :num/2-1;
-    for(int i=0;i<iter;i++)
-    {
-        pt3=pt2->next;
-        pt2->next=pt1;
-        pt1=pt2;
-        pt2=pt3;
-    }
-    if(num%2) pt1=pt1->next;
-    while(pt1&&pt2)
-      if(pt1->val!=pt2->val)
-        return false;
-      else
-      {
-          pt1=pt1->next;
-          pt2=pt2->next;
-      }
-    return true;
+bool isPalindrome(ListNode* head) {
+	ListNode *slow = head, *fast = head, *prev = NULL, *tmp;
+	while(fast && fast -> next) // finding mid point
+		fast = fast -> next -> next,
+		tmp = slow -> next, slow -> next = prev, prev = slow, slow = tmp;        
+	slow = (fast ? slow -> next : slow); // for odd length case as mentioned above
+	while(slow) // check if linked lists starting at prev and slow are equal
+		if(slow -> val != prev -> val) return false;
+		else slow = slow -> next, prev = prev -> next;
+	return true;
 }
 };
