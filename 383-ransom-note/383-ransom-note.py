2c65783ee92cdee1657e2c3a4461a4bfae685d0e
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-08-25 21:17:01 +0530

    Time: 61 ms (86.74%), Space: 14.2 MB (53.79%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,15 +1,23 @@
-class Solution {
-public:
-    bool canConstruct(string a, string b) {
-        int h[26] = {0};
-        for (char &c: b) {
-            ++h[c -'a'];
-        }
-        for (char &c: a) {
-            if (h[c-'a'] == 0) 
-                return 0;
-            --h[c-'a'];
-        }
-        return 1;
-    }
-};
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+      
+        if ransomNote in magazine:
+            return True
+        
+        
+        chars = dict()
+        
+       
+        for i in ransomNote:
+            if i in chars:
+                chars[i] += 1
+            else:
+                chars[i] = 0
+                
+      
+        for i in chars:
+            if magazine.count(i) <= chars[i]: 
+                return False
+            
+        return True
