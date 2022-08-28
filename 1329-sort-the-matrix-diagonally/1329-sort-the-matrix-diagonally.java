64ea92f4a4cdc35a815ff963cd1c188fc0c7d490
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-08-28 11:57:43 +0530

    Time: 2 ms (96.79%), Space: 48 MB (50.43%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,16 +1,31 @@
 class Solution {
-      public int[][] diagonalSort(int[][] A) {
-        int m = A.length, n = A[0].length;
-        HashMap<Integer, PriorityQueue<Integer>> d = new HashMap<>();
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                d.putIfAbsent(i - j, new PriorityQueue<>());
-                d.get(i - j).add(A[i][j]);
-            }
-        }
-        for (int i = 0; i < m; ++i)
-            for (int j = 0; j < n; ++j)
-                A[i][j] = d.get(i - j).poll();
-        return A;
-    }
+	public int[][] diagonalSort(int[][] mat) {
+		//sort row-wise diagonally [00, 10, 20]
+		for(int i=0; i<mat.length; i++) countAndSort(mat, i, 0);
+
+		//sort column-wise diagonally [01, 02, 03]
+		for(int i=0; i<mat[0].length; i++) countAndSort(mat, 0, i);
+
+		return mat;
+	}
+
+	public void countAndSort(int[][] mat, int i, int j) {
+		int[] arr = new int[101];
+		int r = i;
+		int c = j;
+
+		//store the count in arr
+		while(r < mat.length && c < mat[0].length)
+			arr[mat[r++][c++]]++;
+
+		r = i;
+		c = j;
+
+		//loop through [1-100] constraint range and update mat diagonally
+		for(int k=1; k<101; k++) {
+			while(arr[k]-- > 0) {
+				mat[r++][c++] = k;
+			}
+		}
+	}
 }
