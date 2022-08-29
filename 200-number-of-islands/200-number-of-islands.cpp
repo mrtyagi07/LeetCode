845e9c049b5f3fabc18cbcf160dbfe768daf596d
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-08-29 10:34:55 +0530

    Time: 39 ms (88.78%), Space: 12.3 MB (55.87%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
-    void DFS(int i,int j,vector<vector<char>>&grid){
+    int numIslands(vector<vector<char>>& grid) 
+    {
+        int n=grid.size();           // rows
+        int m=grid[0].size();        // col
         
-        int n=grid.size(),m=grid[0].size();
-        if(i<0||i>=n||j<0||j>=m||grid[i][j]=='0'){
-            return;
-        }
-        // cout<<i<<" "<<j<<" "<<grid[i][j]<<endl;
-        vector<pair<int,int>>p={{-1,0},{1,0},{0,-1},{0,1}};
-        grid[i][j]='0';
-        for(int k=0;k<p.size();k++){
-            DFS(i+p[k].first,j+p[k].second,grid);
-        }
-    }
-    int numIslands(vector<vector<char>> grid) {
-        int n=grid.size(),m=grid[0].size();
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                
-                if(grid[i][j]=='1'){
-                    
-                    ans++;
-                    DFS(i,j,grid);
+        // if grid is empty it means no island is there
+        if(n==0) return 0;
+        
+        int res=0;
+        for(int i=0;i<n;i++)                         // iterate through the all cells
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]=='1')                  // if we get cell as value as 1 it means it is the starting of new islands so do res+1 for each new island
+                {
+                    bfs(grid,i,j,n,m);
+                    res+=1;
                 }
             }
         }
-        return ans;
+        return res;
+    }
+    void bfs(vector<vector<char>> &matrix,int x,int y,int r,int c)
+    {
+        //making boundaries 
+        if(x<0 or y<0 or x>=r or y>=c or matrix[x][y]!='1')
+        {
+            return;
+        }
+        
+        //mark as visited
+        matrix[x][y]='2';                               // make currnt cell as 2 for get rid from from overlaping 
+        
+        //cheking all four directions
+        bfs(matrix,x-1,y,r,c);      //top
+        bfs(matrix,x+1,y,r,c);      //down
+        bfs(matrix,x,y-1,r,c);      //left
+        bfs(matrix,x,y+1,r,c);      //right
     }
 };
