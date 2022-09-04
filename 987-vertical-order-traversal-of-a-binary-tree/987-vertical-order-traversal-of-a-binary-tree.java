22690b05d30c9af892b1366d5a1991a8fe6292a6
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-09-04 16:59:41 +0530

    Time: 5 ms (65.62%), Space: 42 MB (99.62%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -1,31 +1,69 @@
 class Solution {
     
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        List<List<Integer>>res = new ArrayList<>();
-        if (root==null)return res;
-        TreeMap<Integer, TreeMap<Integer,List<Integer>>>nodesSorted = new TreeMap<>();
-        dfs(root,nodesSorted,0,0);
-        
-        for (Map.Entry<Integer, TreeMap<Integer,List<Integer>>>entry: nodesSorted.entrySet()){
-            List<Integer>currentList = new ArrayList<>();
-            for (Map.Entry<Integer,List<Integer>>currentEntry: entry.getValue().entrySet()){
-                Collections.sort(currentEntry.getValue());
-                currentList.addAll(currentEntry.getValue());
-            }
-            res.add(currentList);
-        }
-        return res;
+        Map<Integer, List<int[]>> ref = new HashMap<>();
 
+        int[] colWidth = helper(root, 0, 0, ref, new int[] {0, 0});
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        VerticalSortComparator comparator = new VerticalSortComparator();
+
+        for (int i = colWidth[0]; i <= colWidth[1]; i++) {
+            List<int[]> complex = ref.getOrDefault(i, new ArrayList<int[]>());
+            Collections.sort(complex, comparator);
+
+            List<Integer> cur = new ArrayList<>();
+
+            for (int[] c: complex) {
+                cur.add(c[0]);
+            }
+
+            result.add(cur);
+        }
+
+        return result;
+    }
+
+    private int[] helper(TreeNode root, int row, int col, Map<Integer, List<int[]>> ref, int[] colWidth) {
+        if (root == null) {
+            return colWidth;
+        }
+
+        List<int[]> curList = null;
+
+        if (ref.containsKey(col)) {
+            curList = ref.get(col);
+        } else {
+            curList = new ArrayList<>();
+            ref.put(col, curList);
+        }
+
+        curList.add(new int[] {root.val, row});
+
+        if (root.left != null) {
+            int[] ret = helper(root.left, row + 1, col - 1, ref, colWidth);
+            colWidth[0] = Math.min(col - 1, ret[0]);
+        }
+
+        if (root.right != null) {
+            int[] ret = helper(root.right, row + 1, col + 1, ref, colWidth);
+            colWidth[1] = Math.max(col + 1, ret[1]);;
+        }
+
+        return colWidth;
     }
     
-    public void dfs(TreeNode root, TreeMap<Integer, TreeMap<Integer,List<Integer>>>nodesSorted, int level, int order){
-        if (root!=null){
-            nodesSorted.putIfAbsent(order, new TreeMap<>());
-            nodesSorted.get(order).putIfAbsent(level,new ArrayList<Integer>());
-            
-            nodesSorted.get(order).get(level).add(root.val);
-            dfs(root.left, nodesSorted, level+1, order-1);
-            dfs(root.right, nodesSorted, level+1, order+1);
+}
+
+class VerticalSortComparator implements Comparator<int[]> {
+
+    @Override
+    public int compare(int[] o1, int[] o2) {
+        if (o1[1] != o2[1]) {
+            return o1[1] - o2[1];
         }
+
+        return o1[0] - o2[0];
     }
 }
