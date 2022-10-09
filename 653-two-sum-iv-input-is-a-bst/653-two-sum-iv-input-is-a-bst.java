1b333159a479231cf9e45ab473d35099caa0d3b7
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-10-09 17:23:47 +0530

    Time: 2 ms (99.76%), Space: 49.9 MB (80.86%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode root;
+//DFS each node, and try to search the target 'node' such that 'node'.val = k-node.val
+//make sure you don't pick the node itself, like if k = 2 and node.val = 1, don't return node itself!
+public boolean findTarget(TreeNode node, int k) {
+    if(this.root==null) this.root = node;//set variable for the root of this tree
+    if(node==null) return false;
+    if(search(node, k-node.val)) return true;//make sure you don't find the node itself!
+    return findTarget(node.left,k)||findTarget(node.right,k);//DFS traverse
+}
+public boolean search(TreeNode node, int k){
+    TreeNode current = root;//search from the root node
+    while(current!=null){
+        if(k>current.val) current = current.right;
+        else if(k<current.val) current = current.left;
+        else return current!=node?true:false;//you can't find the node itself!
+    }
+    return false;
+}
+
+}
